fa96d1ec209b189aa640c298521a941f811df407
Gustavo Anjos <gustavoanjos42@gmail.com>
2021-02-14 21:06:54 -0400

    Initial Commit

---
+++ ToDoList_SwiftUI+Firebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoList_SwiftUI+Firebase
+//
+//  Created by Gustavo Anjos on 14/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDoList_SwiftUI+Firebase/ToDoList_SwiftUI_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_SwiftUI_FirebaseApp.swift
+//  ToDoList_SwiftUI+Firebase
+//
+//  Created by Gustavo Anjos on 14/02/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_SwiftUI_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
